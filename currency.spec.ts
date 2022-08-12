49028cb009d1a2c077a100a0752acf390cac3371
Alex <awarrington0895@gmail.com>
2022-08-12 12:46:14 +0000

    Eliminated side effect in times method

--- currency.spec.ts
+++ currency.spec.ts
@@ -4,7 +4,12 @@ import { Dollar } from './currency';
 describe('Currency', () => {
     test('multiplication', () => {
         const five = new Dollar(5);
-        five.times(2);
-        expect(five.amount).toBe(10);
+        let product = five.times(2);
+
+        expect(product.amount).toBe(10);
+
+        product = five.times(3);
+
+        expect(product.amount).toBe(15);
     });
 });
--- index.ts
+++
@@ -1,3 +0,0 @@
-export class Dollar {
-    constructor(private readonly amount: number) {}
-}

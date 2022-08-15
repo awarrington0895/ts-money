f19d25c04776f08a2d915714d30ef23759e69a91
Alex <awarrington0895@gmail.com>
2022-08-15 14:14:16 +0000

    Switch all dollar references to use static factory method

--- money.spec.ts
+++ money.spec.ts
@@ -2,22 +2,27 @@ import { describe, test, expect } from 'vitest';
 import { Money, Franc, Dollar } from './money';
 
 describe('Money', () => {
+    const usd = 'USD';
+    const chf = 'CHF';
+
+    test('equality', () => {
+        expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+        expect(new Dollar(5, usd).equals(Money.dollar(6))).toBe(false);
+        expect(new Franc(5, chf).equals(new Franc(5, chf))).toBe(true);
+        expect(new Franc(5, chf).equals(new Franc(6, chf))).toBe(false);
+        expect(new Franc(5, chf).equals(Money.dollar(5))).toBe(false);
+    });
+
+    test('equals should work with object that may not be a dollar', () => {
+        expect(Money.dollar(5).equals(10)).toBe(false);
+    });
+
     describe('Franc', () => {
-        const usd = 'USD';
-        const chf = 'CHF';
 
         test('francMultiplication', () => {
             const five = new Franc(5, chf);
             expect(five.times(2).equals(new Franc(10, chf))).toBe(true);
             expect(five.times(3).equals(new Franc(15, chf))).toBe(true);
-        });
-
-        test('equals', () => {
-            expect(new Dollar(5, usd).equals(new Dollar(5, usd))).toBe(true);
-            expect(new Dollar(5, usd).equals(new Dollar(6, usd))).toBe(false);
-            expect(new Franc(5, chf).equals(new Franc(5, chf))).toBe(true);
-            expect(new Franc(5, chf).equals(new Franc(6, chf))).toBe(false);
-            expect(new Franc(5, chf).equals(new Dollar(5, usd))).toBe(false);
         });
     });
 
@@ -30,12 +35,6 @@ describe('Money', () => {
             expect(five.times(3).equals(Money.dollar(15))).toBe(true);
         });
 
-        test('equality', () => {
-            expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
-        });
-
-        test('equals should work with object that may not be a dollar', () => {
-            expect(Money.dollar(5).equals(10)).toBe(false);
-        });
+        
     });
 });

5c55b660b58e9cd92e8afd0eab618497d4ab423a
Alex <awarrington0895@gmail.com>
2022-08-15 14:18:23 +0000

    Create static factory for franc

--- money.spec.ts
+++ money.spec.ts
@@ -1,15 +1,13 @@
-import { describe, test, expect } from 'vitest';
-import { Money, Franc, Dollar } from './money';
+import { describe, expect, test } from 'vitest';
+import { Money } from './money';
 
 describe('Money', () => {
-    const chf = 'CHF';
-
     test('equality', () => {
         expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
         expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
-        expect(new Franc(5, chf).equals(new Franc(5, chf))).toBe(true);
-        expect(new Franc(5, chf).equals(new Franc(6, chf))).toBe(false);
-        expect(new Franc(5, chf).equals(Money.dollar(5))).toBe(false);
+        expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
+        expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
+        expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
     });
 
     test('equals should work with object that may not be a dollar', () => {
@@ -18,9 +16,11 @@ describe('Money', () => {
 
     describe('Franc', () => {
         test('francMultiplication', () => {
-            const five = new Franc(5, chf);
-            expect(five.times(2).equals(new Franc(10, chf))).toBe(true);
-            expect(five.times(3).equals(new Franc(15, chf))).toBe(true);
+            const five = Money.franc(5);
+
+            expect(five.times(2).equals(Money.franc(10))).toBe(true);
+
+            expect(five.times(3).equals(Money.franc(15))).toBe(true);
         });
     });
 
